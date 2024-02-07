87279af5170a207aee81976f8091c35a5b58f9b3
Aria <159058759+Ariadesiner@users.noreply.github.com>
2024-02-07 06:37:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let famillypics = document.querySelector('.picturein')
+let famillypics2 = document.querySelector('.familly2')
+let famillypics3 = document.querySelector('.picsqure')
+let famillypics4 = document.querySelector('.piccircle')
+let famillypics5 = document.querySelector('.picturein')
+let famillypics6 = document.querySelector('.largpic')
+let famillypics7 = document.querySelector('.familly3')
+let famillypics8 = document.querySelector('.container')
+function clickme(){
+    famillypics2.style.display = 'block'
+    famillypics4.style.display = 'none'
+    famillypics5.style.display = 'none'
+}
+function clickme2(){
+    famillypics2.style.display = 'none'
+    famillypics3.style.display = 'none'
+    famillypics4.style.display = 'none'
+    famillypics5.style.display = 'none'
+    famillypics8.style.display = 'none'
+    famillypics7.style.display = 'block'
+}
+function clickme3(){
+    
+}
